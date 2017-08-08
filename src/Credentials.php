4093c5a42ca5e49aadb71ad27e37b18e79dfd6ec
Ronaldo M. Rodrigues <ronaldo@whera.com.br>
2017-08-08 19:17:00 -0300

    create class credentials

---
+++ src/Credentials.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace WSW\SiftScience;
+
+use WSW\SiftScience\Environments\Environment;
+use WSW\SiftScience\Environments\Production;
+
+/**
+ * Class Credentials
+ *
+ * @package WSW\SiftScience
+ * @author Ronaldo Matos Rodrigues <ronaldo@whera.com.br>
+ */
+class Credentials
+{
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    /**
+     * @var Environment
+     */
+    private $environment;
+
+    /**
+     * @param string $apiKey
+     * @param Environment $environment
+     */
+    public function __construct($apiKey, Environment $environment = null)
+    {
+        $this->apiKey = substr($apiKey, 0, 16);
+        $this->environment = $environment ?: new Production();
+    }
+
+    /**
+     * @param string $resource
+     *
+     * @return string
+     */
+    public function getUrl($resource)
+    {
+        return $this->environment->getUrl($resource);
+    }
+
+    /**
+     * @param string $resource
+     * @param array $params
+     *
+     * @return string
+     */
+    public function getWsUrl($resource, array $params = [])
+    {
+        if (empty($params)) {
+            return $this->environment->getWsUrl($resource);
+        }
+
+        return sprintf('%s?%s', $this->environment->getWsUrl($resource), http_build_query($params));
+    }
+}
---
+++ tests/CredentialsTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace WSW\SiftScience;
+
+use WSW\SiftScience\Environments\Environment;
+use WSW\SiftScience\Environments\Production;
+
+/**
+ * Class CredentialsTest
+ *
+ * @package WSW\SiftScience
+ * @author Ronaldo Matos Rodrigues <ronaldo@whera.com.br>
+ */
+class CredentialsTest extends TestCase
+{
+    /**
+     * @var Environment
+     */
+    private $environment;
+
+    protected function setUp()
+    {
+        $this->environment = $this->getMockForAbstractClass(Environment::class);
+        $this->environment->expects($this->any())
+                          ->method('getHost')
+                          ->willReturn('test.com');
+        $this->environment->expects($this->any())
+                          ->method('getWsHost')
+                          ->willReturn('ws.test.com');
+    }
+
+    /**
+     * @test
+     */
+    public function constructShouldConfigureTheAttributes()
+    {
+        $credentials = new Credentials('1234567890123654', $this->environment);
+        $this->assertAttributeEquals('1234567890123654', 'apiKey', $credentials);
+        $this->assertAttributeSame($this->environment, 'environment', $credentials);
+    }
+
+    /**
+     * @test
+     */
+    public function constructShouldTruncateApiKey()
+    {
+        $credentials = new Credentials(str_repeat('a', 32), $this->environment);
+        $this->assertAttributeEquals(str_repeat('a', 16), 'apiKey', $credentials);
+    }
+
+    /**
+     * @test
+     */
+    public function constructShouldUseProductionAsDefaultEnvironment()
+    {
+        $credentials = new Credentials('1234567890123654');
+        $this->assertAttributeInstanceOf(Production::class, 'environment', $credentials);
+    }
+
+    /**
+     * @test
+     */
+    public function getUrlShouldGetTheUrlFromTheEnvironment()
+    {
+        $credentials = new Credentials('1234567890123654', $this->environment);
+        $this->assertEquals('https://test.com/test', $credentials->getUrl('/test'));
+    }
+
+    /**
+     * @test
+     */
+    public function getWsUrlShouldGetTheWsUrlFromTheEnvironmentAppendingParams()
+    {
+        $credentials = new Credentials('1234567890123654', $this->environment);
+
+        $this->assertEquals('https://ws.test.com/test?page=1', $credentials->getWsUrl('/test', ['page' => '1']));
+    }
+}
