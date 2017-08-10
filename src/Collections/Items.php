f96d6723b970c734cd4efa06e864873ee614c2b6
Ronaldo M. Rodrigues <ronaldo@whera.com.br>
2017-08-10 11:34:19 -0300

    create collection

---
+++ src/Collections/Items.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WSW\SiftScience\Collections;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use WSW\Money\Money;
+use WSW\SiftScience\Entities\Item;
+
+/**
+ * Class Items
+ *
+ * @package WSW\SiftScience\Collections
+ * @author Ronaldo Matos Rodrigues <ronaldo@whera.com.br>
+ */
+class Items extends ArrayCollection
+{
+}
---
+++ src/Collections/Promotions.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WSW\SiftScience\Collections;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Class Promotions
+ *
+ * @package WSW\SiftScience\Collections
+ * @author Ronaldo Matos Rodrigues <ronaldo@whera.com.br>
+ */
+class Promotions extends ArrayCollection
+{
+}
